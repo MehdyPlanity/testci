0d75ce3b05a4ee6c7de6d051bb61ed2281ba5b02
Mehdy <mehdy.chelabi@planity.com>
2022-08-12 11:59:41 +0200

    fix: add commitizen for test

---
+++ main.js
@@ -0,0 +1,13 @@
+
+const moment= require('moment')
+
+function main(){
+for(let i =0; i<10 ;i++){
+    console.log('compteur',i+1)
+    console.log('moment',moment().format("DD/MM"))
+    console.log('hey')
+}
+}
+
+
+main()
