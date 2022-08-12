b0d847c1fac9c869c82c91c7c02b5556ba47b412
Mehdy <mehdy.chelabi@planity.com>
2022-08-12 15:42:55 +0200

    anh jo

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const moment = require("moment");
 function main() {
   for (let i = 0; i < 10; i++) {
     console.log("compteur", i + 1);
-    console.log("moment", moment().format("DD/MM"));
-    console.log("hey  ho!!");
+                   console.log("moment", moment().format("DD/MM"));
+                 console.log("hey                  ho!!");
   }
 }
 
